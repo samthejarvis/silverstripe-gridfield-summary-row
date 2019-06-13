6fb90300103545f6655043a017ed0291977215db
Morven Lewis-Everley <morven@i-lateral.com>
2019-06-13 12:17:20 +0100

    PHPCBF

--- src/GridFieldSummaryRow.php
+++ src/GridFieldSummaryRow.php
@@ -8,50 +8,56 @@ use SilverStripe\View\Requirements;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Forms\GridField\GridField_HTMLProvider;
 
-class GridFieldSummaryRow implements GridField_HTMLProvider 
+class GridFieldSummaryRow implements GridField_HTMLProvider
 {
 
-	public function __construct($fragment = 'footer') 
-	{
-		$this->fragment = $fragment;
-	}
+    public function __construct($fragment = 'footer') 
+    {
+        $this->fragment = $fragment;
+    }
 
-	function getHTMLFragments($gridField) 
-	{
-		Requirements::css("samthejarvis/gridfield-summary-row:client/css/summary-row.css");
+    function getHTMLFragments($gridField) 
+    {
+        Requirements::css("samthejarvis/gridfield-summary-row:client/css/summary-row.css");
 
-		$columns = $gridField->getColumns();
-		$list = $gridField->getList();
+        $columns = $gridField->getColumns();
+        $list = $gridField->getList();
 
-		$summary_values = new ArrayList();
+        $summary_values = new ArrayList();
 
-		foreach($columns as $column) {
-			$singleton = Injector::inst()->get($list->dataClass, true);
-			$db = $singleton->config()->db;
-			if(singleton($list->dataClass)->hasField($column)){
-				if($db[$column] == "Money") {
-					$summary_value = $list->sum($column."Amount");
-				} else {
-					$summary_value = $list->sum($column);
-				}
-	        }
-	        else
-	        {
-	        	$summary_value = "";
-	        }
+        foreach($columns as $column) {
+            $singleton = Injector::inst()->get($list->dataClass, true);
+            $db = $singleton->config()->db;
+            if(singleton($list->dataClass)->hasField($column)) {
+                if($db[$column] == "Money") {
+                    $summary_value = $list->sum($column."Amount");
+                } else {
+                    $summary_value = $list->sum($column);
+                }
+            }
+            else
+            {
+                $summary_value = "";
+            }
 
-	        $summary_values->push(new ArrayData(array(
-				"Value" => $summary_value
-			)));
-			
-		}
+            $summary_values->push(
+                new ArrayData(
+                    array(
+                    "Value" => $summary_value
+                    )
+                )
+            );
+            
+        }
 
-		$data = new ArrayData(array(
-			'SummaryValues' => $summary_values
-		));
+        $data = new ArrayData(
+            array(
+            'SummaryValues' => $summary_values
+            )
+        );
 
-		return array(
-			$this->fragment => $data->renderWith(__CLASS__)
-		);
-	}
+        return array(
+        $this->fragment => $data->renderWith(__CLASS__)
+        );
+    }
 }
